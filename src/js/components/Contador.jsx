a68a9a6684a27978e8a5b8a214c67b1b056c1739
Pascual Rivera M <priveram87@gmail.com>
2025-06-26 23:45:54 +0000

    fix responsive css

--- src/js/components/Contador.jsx
+++ src/js/components/Contador.jsx
@@ -3,18 +3,27 @@ import React from "react";
 const Contador = (props) => {
   return (
     <>
-      <div id="Content" className="container-fluid">
-        <div className="d-flex flex-wrap justify-content-center Contador">
-          <div id="clockIcon" className="numero"><i className="fa-regular fa-clock"></i></div>
-          <div className="numero">{props.numero6}</div>
-          <div className="numero">{props.numero5}</div>
-          <div className="numero">{props.numero4}</div>
-          <div className="numero">{props.numero3}</div>
-          <div className="numero">{props.numero2}</div>
-          <div className="numero">{props.numero1}</div>
-          <button id="stop" onClick={props.onStop}>STOP</button>
-          <button id="start" onClick={props.onStart}>START</button>
-          <button id="restart" onClick={props.onRestart}>RESTART</button>
+      <div className="sawContainer">
+        <div id="Content" className="container-fluid">
+          <div className="d-flex flex-column align-items-center Contador">
+            <div className="d-flex flex-row justify-content-center numeros-container">
+              <div id="clockIcon" className="numero">
+                <i className="fa-regular fa-clock"></i>
+              </div>
+              <div className="numero">{props.numero6}</div>
+              <div className="numero">{props.numero5}</div>
+              <div className="numero">{props.numero4}</div>
+              <div className="numero">{props.numero3}</div>
+              <div className="numero">{props.numero2}</div>
+              <div className="numero">{props.numero1}</div>
+            </div>
+
+            <div className="botones mt-3 d-flex flex-wrap justify-content-center">
+              <button id="stop" onClick={props.onStop}>STOP</button>
+              <button id="start" onClick={props.onStart}>START</button>
+              <button id="restart" onClick={props.onRestart}>RESTART</button>
+            </div>
+          </div>
         </div>
       </div>
     </>
